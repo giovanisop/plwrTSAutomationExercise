4dae857d1fe431e097b2eab1da58f0a224fb5aa3
Giovani Ouro Preto <giovaniouropreto@gmail.com>
2026-06-30 20:53:50 -0300

    commented .env setting not used right now

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
-import dotenv from 'dotenv';
+//import dotenv from 'dotenv';
 
-dotenv.config();
+//dotenv.config();
 
 export default defineConfig({
   testDir: './tests',
